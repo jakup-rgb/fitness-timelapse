5d6423c01f64a8779e583c1cd2e70ce506ae53bb
Muhamed <jakup.m02@gmail.com>
2026-02-17 16:00:55 +0100

    fix?

--- app/camera/page.tsx
+++ app/camera/page.tsx
@@ -108,8 +108,8 @@ export default function CameraPage() {
            "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.14/wasm"
         );
 
-        // Model liegt in /public/models/face_detector.task
-        const modelPath = "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/1/blaze_face_short_range.task?alt=media";
+        // Model liegt in 
+        const modelPath = "https://storage.googleapis.com/mediapipe-models/face_detector/blaze_face_short_range/float16/latest/blaze_face_short_range.tflite";
 
         // Versuch GPU, fallback CPU (auf manchen Geräten zickt GPU)
         try {
